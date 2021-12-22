6866c69c22568b58d1d25cafdea77cc813e87985
kahina.lounis <kahina.lounis@europcar.com>
2021-12-22 11:40:59 +0100

    Initial Commit

---
+++ Shared/Projet1SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet1SwiftUIApp.swift
+//  Shared
+//
+//  Created by Kahina Lounis on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Projet1SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
